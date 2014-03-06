e81af8233744d61b0e356c0fb6d39246834c8d59
mnskchg <k.c.h.gan@bath.ac.uk>
2014-03-06 15:24:05 +0000

    Added a chart

--- content.js
+++ content.js
@@ -66,8 +66,37 @@ boxoutContent.appendChild(newElement);
 //Stick the container div into the body of the page
 bodyElement.insertBefore(boxoutDiv, bodyElement.firstChild);
 
-// var chartElement = document.createElement('canvas');
-// bodyElement.appendChild(chartElement);
+
+// Chart
+var data = {
+    labels : ["January","February","March","April","May","June","July"],
+    datasets : [
+        {
+            fillColor : "rgba(220,220,220,0.5)",
+            strokeColor : "rgba(220,220,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+            data : [65,59,90,81,56,55,40]
+        },
+        {
+            fillColor : "rgba(151,187,205,0.5)",
+            strokeColor : "rgba(151,187,205,1)",
+            pointColor : "rgba(151,187,205,1)",
+            pointStrokeColor : "#fff",
+            data : [28,48,40,19,96,27,100]
+        }
+    ]
+}
+
+var chartElement = document.createElement('canvas');
+chartElement.setAttribute('id', 'myChart');
+chartElement.setAttribute('width', '400');
+chartElement.setAttribute('height', '400');
+boxoutContent.appendChild(chartElement);
+
+var context = chartElement.getContext('2d');
+new Chart(context).Line(data);
+
 
 // window.addEventListener("load", function() {
 //     console.log('Sending Url: ' + url);
