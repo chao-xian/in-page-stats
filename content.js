60dc75855dc3e9c63f55e52cfc324ef5042e04de
chao-xian <dev@kelvingan.co.uk>
2014-04-23 17:01:41 +0100

    Now renders a pie chart from data that's fetched

--- content.js
+++ content.js
@@ -14,21 +14,113 @@ xhr.onreadystatechange = function() {
         data = JSON.parse(xhr.responseText);
 
         console.log(data);
+        console.log(data.data);
 
+        // Chart
+/*        var chartData = {
+            labels : ["January","February","March","April","May","June","July"],
+            datasets : [
+                {
+                    fillColor : "rgba(220,220,220,0.5)",
+                    strokeColor : "rgba(220,220,220,1)",
+                    pointColor : "rgba(220,220,220,1)",
+                    pointStrokeColor : "#fff",
+                    data : [65,59,90,81,56,55,40]
+                },
+                {
+                    fillColor : "rgba(151,187,205,0.5)",
+                    strokeColor : "rgba(151,187,205,1)",
+                    pointColor : "rgba(151,187,205,1)",
+                    pointStrokeColor : "#fff",
+                    data : [28,48,40,19,96,27,100]
+                }
+            ]
+        };*/
         // Iterate over each item in the JSON object and shove into our container div
         // data.data.forEach(function(object) {
-        for (var i=0; i < 6; i++) {
-            var dataElement = document.createElement('p');
-            // var dataText = document.createTextNode(object.institution);
-            var dataText = document.createTextNode(data.data[i].institution);
-            dataElement.appendChild(dataText);
-            boxoutContent.appendChild(dataElement);
-        }
+        // for (var i=0; i < 6; i++) {
+        //     var dataElement = document.createElement('p');
+        //     // var dataText = document.createTextNode(object.institution);
+        //     var dataText = document.createTextNode(data.data[i].mobile);
+        //     dataElement.appendChild(dataText);
+        //     boxoutContent.appendChild(dataElement);
+        // }
+
+        var chartData = {};
+
+        // for (var i in data) {
+        //     labels.push(data[i].Week);
+        // }
+        var dataSets = [];
+        /*for (var dataEntry in data.data) {
+            console.log(data.data[dataEntry]);
+            var stats = data.data[dataEntry];
+            // TODO: Parse data to push into chartData here
+            dataSets.push(
+                {
+                    value: stats.mobile,
+                    color:"#F38630"
+                },
+                {
+                    value: stats.tablet,
+                    color : "#E0E4CC"
+                },
+                {
+                    value: stats.desktop,
+                    color : "#69D2E7"
+                }
+
+            );
+        }*/
+        dataSets.push(
+            {
+                value: parseInt(data.data[0].mobile, 10),
+                color: "#F38630"
+            },
+            {
+                value: parseInt(data.data[0].tablet, 10),
+                color : "#E0E4CC"
+            },
+            {
+                value: parseInt(data.data[0].desktop, 10),
+                color : "#69D2E7"
+            }
+        );
+
+/*        var data = [
+            {
+                value: 30,
+                color:"#F38630"
+            },
+            {
+                value : 50,
+                color : "#E0E4CC"
+            },
+            {
+                value : 100,
+                color : "#69D2E7"
+            }
+        ];*/
+
+        // chartData.labels = ["Mobile", "Tablet", "Desktop"];
+        chartData.datasets = dataSets;
+        console.log(chartData);
+
+        var chartElement = document.createElement('canvas');
+        chartElement.setAttribute('id', 'myChart');
+        chartElement.setAttribute('width', '400');
+        chartElement.setAttribute('height', '400');
+        boxoutContent.appendChild(chartElement);
+
+        var context = chartElement.getContext('2d');
+        new Chart(context).Pie(dataSets);
+
     }
 };
 
 // Send the AJAX request with no data
-xhr.open('get', 'http://opendatapress.appspot.com/craig552uk/uk-uni-social-media-accounts.json');
+// xhr.open('get', 'http://opendatapress.appspot.com/craig552uk/uk-uni-social-media-accounts.json');
+xhr.open('get', 'http://opendatapress.appspot.com/bathweb/platform.json');
 xhr.send({});
 
 // Get the body element
@@ -65,37 +157,6 @@ boxoutContent.appendChild(newElement);
 
 //Stick the container div into the body of the page
 bodyElement.insertBefore(boxoutDiv, bodyElement.firstChild);
-
-
-// Chart
-var data = {
-    labels : ["January","February","March","April","May","June","July"],
-    datasets : [
-        {
-            fillColor : "rgba(220,220,220,0.5)",
-            strokeColor : "rgba(220,220,220,1)",
-            pointColor : "rgba(220,220,220,1)",
-            pointStrokeColor : "#fff",
-            data : [65,59,90,81,56,55,40]
-        },
-        {
-            fillColor : "rgba(151,187,205,0.5)",
-            strokeColor : "rgba(151,187,205,1)",
-            pointColor : "rgba(151,187,205,1)",
-            pointStrokeColor : "#fff",
-            data : [28,48,40,19,96,27,100]
-        }
-    ]
-}
-
-var chartElement = document.createElement('canvas');
-chartElement.setAttribute('id', 'myChart');
-chartElement.setAttribute('width', '400');
-chartElement.setAttribute('height', '400');
-boxoutContent.appendChild(chartElement);
-
-var context = chartElement.getContext('2d');
-new Chart(context).Line(data);
 
 
 // window.addEventListener("load", function() {
